27f6ff7161cea7964745a623e528264798d817d8
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-12-12 10:54:11 -0800

    update table

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals ( 
-    id int NOT NULL PRIMARY KEY, 
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, 
     name varchar(100), 
     date_of_birth date, 
     escape_attempts int, 
